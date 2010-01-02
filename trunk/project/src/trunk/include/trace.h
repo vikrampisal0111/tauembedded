a9deb8ea054b8fda5f42a08af01997c4c978e518
genged <genged@c82d262a-d0f4-11de-a205-5990a31c5447>
2010-01-02 18:25:59 +0000

    Initial import.  * lcd and uart0 are working.  * stdio integration working, which means we can use printf for uart output  * Makefile based build system is working but needs some changes

---
+++ trunk/project/src/trunk/include/trace.h
@@ -0,0 +1,37 @@
+#ifndef __TRACE_H__
+#define __TRACE_H__
+
+#if UART0_DEBUG == 1 || UART1_DEBUG == 1
+        #include <stdio.h>
+        #define TRACE(X ,reg...)   \
+                do {	           \
+                printf("\r\n");    \
+                printf(X, ##reg);  \
+                } while(0)
+
+        #define trace(X ,reg...)     \
+                do {	             \
+                printf(X, ##reg);    \
+                } while(0)
+
+        #define ASSERT(expr)               \
+                do{                        \
+                        if (expr) { ; }    \
+                        else  {            \
+                        while (1){	   \
+                        printf("\r\nAssert failed: " #expr " (file %s line %d)", \
+                                 __FILE__, (int) __LINE__ );                     \
+                        }                  \
+                        }                  \
+                } while(0)
+
+        #define EP()  TRACE("%s Line %d", __FILE__, (int) __LINE__) ;
+#else
+        #define TRACE_INIT() 	  do{}while(0)
+        #define ASSERT(X) 	  do{}while(0)
+        #define TRACE(X, reg...)  do{}while(0)
+        #define trace(X, reg...)  do{}while(0)
+        #define EP()  	          do{}while(0)
+#endif
+
+#endif /* __TRACE_H_  */
