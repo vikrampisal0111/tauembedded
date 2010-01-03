1bd88eb5614b69e4cefe7f1db7e33ab8540d59a6
wieyal <wieyal@c82d262a-d0f4-11de-a205-5990a31c5447>
2010-01-03 14:23:02 +0000

    Changed to eyals spi driver

--- trunk/project/src/trunk/arch/lpc21xx/spi/spi.h
+++ trunk/project/src/trunk/arch/lpc21xx/spi/spi.h
@@ -1,34 +1,12 @@
-/*
-* Name: SPI_MMC.H
-* Purpose: SPI mode SD/MMC card interface driver
-* Version: V1.03
-* Copyright (c) 2006 NXP Semiconductor. All rights reserved.
-*---------------------------------------------------------------------*/
+#ifndef _SPI_H_
+#define _SPI_H_
 
-#ifndef __SPI_MMC_H__
-#define __SPI_MMC_H__
+/* SPI select pin */ 
+#define SPI_SEL      (1 << 11) 
 
-/* SPI select pin */
-#define SPI_SEL 0x00100000
+void SPI_Init( void ); 
+void SPI_Send( BYTE *Buf, DWORD Length ); 
+void SSP_SendRecvByte( BYTE *Buf, DWORD Length ); 
+BYTE SSP_SendRecvByteByte( void ); 
 
-/* The max MMC flash size is 256MB */
-#define MAX_TIMEOUT 0xFF
-#define IDLE_STATE_TIMEOUT
-
-#define OP_COND_TIMEOUT 2
-#define SET_BLOCKLEN_TIMEOUT 3
-#define WRITE_BLOCK_TIMEOUT 4
-#define WRITE_BLOCK_FAIL 5
-#define READ_BLOCK_TIMEOUT 6
-#define READ_BLOCK_DATA_TOKEN_MISSING 7
-#define DATA_TOKEN_TIMEOUT 8
-#define SELECT_CARD_TIMEOUT 9
-#define SET_RELATIVE_ADDR_TIMEOUT 10
-
-void SPI_Init( void );
-void SPI_Send( BYTE *Buf, DWORD Length );
-uint32_t SPI_TxRx(uint32_t out);
-void SPI_Receive( BYTE *Buf, DWORD Length );
-BYTE SPI_ReceiveByte( void );
-
-#endif /* __SPI_MMC_H__ */
+#endif //_SPI_H_
