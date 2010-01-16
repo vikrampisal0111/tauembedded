ba36d3ac5fa59308e9ab3d00932c9281e1aae0ed
genged <genged@c82d262a-d0f4-11de-a205-5990a31c5447>
2010-01-16 12:15:53 +0000

    * Small cleanup  * adapted main.c to hello-world app

--- trunk/project/src/trunk/main.c
+++ trunk/project/src/trunk/main.c
@@ -1,46 +1,72 @@
 #include <stdio.h>
 
-#include "lpc214x.h"                        /* LPC21xx definitions */
+#include "lpc214x.h"
 #include "type.h"
-//#include "irq.h"
-//#include "ssp.h"
 #include "uart0.h"
 
 #include "network.h"
-#include "enc28j60.h"
 
-//BYTE SPIWRData[BUFSIZE];
-//BYTE SPIRDData[BUFSIZE];
-//DWORD CurrentTxIndex = 0;
-//DWORD CurrentRxIndex = 0;
+// TODO Remove this after finished exprimenting & debugging
+#include "enc28j60.h"
+#include "uip.h"
+#include "uip_arp.h"
+#include "timer.h"
+#include "hello-world.h"
+
+void uip_log(char *m)
+{
+  printf("uIP log message: %s\n", m);
+}
 
 int main(void)
 {
     unsigned int i;
-    uint8_t macaddr[6] = { 0x00, 0xf8, 0xc1, 0xd8, 0xc7, 0xa6 };
+    uip_eth_addr macaddr = { 
+	.addr = { 0x00, 0xf8, 0xc1, 0xd8, 0xc7, 0xa6} 
+    };
 
+    uip_ipaddr_t ipaddr;
+    struct timer periodic_timer, arp_timer;
+
+    timer_set(&periodic_timer, CLOCK_SECOND / 2);
+    timer_set(&arp_timer, CLOCK_SECOND * 10);
+ 
     VPBDIV = 0x02;
 
-//    init_VIC();
     uart0Init();
     printf("Uart Init\n");
-    fflush(stdout);
-
-    //return 0;
 
     network_init();
     printf("network init\n");
+ 
+    uip_init();
+    printf("uIP init\n");
 
-    printf("Started\n");
-    fflush(stdout);
+    uip_setethaddr(macaddr);
+    printf("Setting MAC address '%x:%x:%x:%x:%x:%x' with uIP\n", 
+	    macaddr.addr[0], macaddr.addr[1], macaddr.addr[2], macaddr.addr[3], macaddr.addr[4],macaddr.addr[5]);
 
-    enc28j60_set_mac_address(macaddr);
-    printf("Set mac addr\n");
     for(i = 0; i < 6; i++) {
-	macaddr[i] = 0;
+	macaddr.addr[i] = 0;
     }
-    enc28j60_get_mac_address((uint8_t *)&macaddr);
-    printf("mac==%x:%x:%x:%x:%x:%x", macaddr[0], macaddr[1], macaddr[2], macaddr[3], macaddr[4],macaddr[5]);
-    fflush(stdout);
+
+    enc28j60_get_mac_address((uint8_t *)&(macaddr.addr));
+    printf("Testing using driver: MAC == %x:%x:%x:%x:%x:%x", 
+	    macaddr.addr[0], macaddr.addr[1], macaddr.addr[2], macaddr.addr[3], macaddr.addr[4],macaddr.addr[5]);
+
+    uip_ipaddr(ipaddr, 12,12,12,13);
+    uip_sethostaddr(ipaddr);
+    printf("Set host IP addr to: %d.%d.%d.%d", 12,12,12,13);
+
+    uip_ipaddr(ipaddr, 12,12,12,12);
+    uip_setdraddr(ipaddr);
+    printf("Set my ip addr to: %d.%d.%d.%d", 12,12,12,12);
+
+    uip_ipaddr(ipaddr, 255,255,255,0);
+    uip_setnetmask(ipaddr);
+
+    hello_world_init();
+    while(1);
+
     return 0;
 }
